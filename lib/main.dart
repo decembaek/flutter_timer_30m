c5cf25c67afd306bf346fa6e5a957588d54cffef
decembaek <tmdrbpp123@gmail.com>
2023-11-05 19:18:50 +0900

    flutter timer

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pomo_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// ColorScheme(background: Color(0xFFE7627C))
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            backgroundColor: const Color(0xFFE7626C),
+          ),
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
+            ),
+          ),
+          cardColor: const Color(0xFFF4EDDB),
+        ),
+        home: const HomeScreen());
+  }
+}
